2e765a6f7566c22adf5f68ddbaa82ed89ca70ed1
johnjp15 <johnjp15@gmail.com>
2017-06-21 16:34:39 -0400

    Fixed loadcfg.sh to create the path .vim/colors

--- loadcfg.sh
+++ loadcfg.sh
@@ -5,6 +5,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 #ls $vimcfgdir 
+\mkdir -p ~/.vim/colors
 \cp $SCRIPTPATH/vimcfgfile ~/.vimrc -b
 \cp $SCRIPTPATH/monokai.vim ~/.vim/colors -b
 echo "Loaded .vimrc file from $SCRIPTPATH"
