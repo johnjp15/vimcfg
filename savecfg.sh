83285c09fb2c344a233ffb7fe2679095dbd1472f
John Park <jhnpark@amazon.com>
2018-08-10 06:06:38 +0000

    Updated savecfg.sh for .tmux.conf

--- savecfg.sh
+++ savecfg.sh
@@ -7,4 +7,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 #ls $vimcfgdir 
 \cp ~/.vimrc $SCRIPTPATH/vimcfgfile -b
 \cp -rfb ~/.vim/colors $SCRIPTPATH/.
+
+
+TMUXPATH="$SCRIPTPATH/tmux"
+
+cp -b ~/.tmux.conf $TMUXPATH/tmuxconf
+
 echo "Saved configuration files to $SCRIPTPATH"
